95e58ab9455e7be5ca2d00ca3169a04be28ba619
林佳志乃 <twinklestaryoshino@gmail.com>
2019-01-02 13:07:00 +0900

    fix v-model

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello vue.js!'
-    toggle: true,
-    object: {
-      firstName: 'Hayashi',
-      lastName: 'Yoshino'
-    }
-  },
-  methods: {
-    say: function (message) {
-      alert(message)
-    }
+    message: 'Hello Vue.js!'
   }
 })
+
