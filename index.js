669bc9cb3649e1f90855bb3ae741ae293d3410d1
林佳志乃 <twinklestaryoshino@gmail.com>
2019-01-06 20:10:53 +0900

    create addbuttonvbind

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-Vue.component('hello-component', { template: '<p>HELLO</p>'})
+Vue.component('hello-component', {template: '<p>HELLO</p>'})
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    newItem: ''
-  },
-  methods: {
-    addItem: function(event) {
-      alert();
+    el: '#app',
+    data: {
+        newItem: '',
+        todos: []
+    },
+    methods: {
+        addItem: function (event) {
+            alert();
+        }
     }
-  }
 })
 
