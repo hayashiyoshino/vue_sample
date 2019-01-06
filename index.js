4b5f305eb6f0a1486203552ec72353e0fe201759
林佳志乃 <twinklestaryoshino@gmail.com>
2019-01-06 20:18:16 +0900

    definition addItem function

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 Vue.component('hello-component', {template: '<p>HELLO</p>'})
 
 var app = new Vue({
-    el: '#app',
-    data: {
-        newItem: '',
-        todos: []
-    },
-    methods: {
-        addItem: function (event) {
-            alert();
-        }
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function (event) {
+      if (this.newItem == "") return;
+      var todo = {
+        item: this.newItem,
+        isDone: false
+      };
+      this.todos.push(todo);
+      this.newItem = "";
     }
+  }
 })
 
